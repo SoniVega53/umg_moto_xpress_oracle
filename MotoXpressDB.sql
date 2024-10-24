b11dd57f9283bca3cf201ccabb5a1ded213e68a3
MAAD03 <mescobarc20@miumg.edu.gt>
2024-10-23 21:42:48 -0600

    ORACLE
    Script ya modificado para tener estructura de oracle y no de mysql.

--- MotoXpressDB.sql
+++ MotoXpressDB.sql
@@ -36,142 +36,145 @@ END;
 
 
 
---NO se como implementar lo que hiciste arriba entonces voy a empzar desde cero para realizar lo basico del script
---Script para base de datos para proyecto final.
+-- Script para base de datos para proyecto final en Oracle
 
-CREATE DATABASE BD_ProyectoFinal;
-use BD_ProyectoFinal;
+CREATE SEQUENCE rol_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE bitacora_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE persona_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE usuario_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE modelo_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE marca_modelo_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE imagen_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE tipo_estado_motocicleta_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE motocicleta_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE zona_geografica_seq START WITH 1 INCREMENT BY 1;
 
+CREATE TABLE ROL (
+    id_rol NUMBER PRIMARY KEY,
+    nombre VARCHAR2(50),
 
- /* 
- CREATE TABLE NOMBRE (
-
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
-);
-
-*/
-
-
-create table ROL (
-id_rol INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR2(50),
-
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
 );
 
 CREATE TABLE BITACORA (
-id_bitacora INT AUTO_INCREMENT PRIMARY KEY,
-tabla_afectada VARCHAR2(50),
-tipo_operacion VARCHAR2(50),
-detalle_de_accion VARCHAR2(50),
-fecha_hora DATETIME,
-datos_anteriores VARCHAR2(250),
-datos_nuevos VARCHAR2(250),
+    id_bitacora NUMBER PRIMARY KEY,
+    tabla_afectada VARCHAR2(50),
+    tipo_operacion VARCHAR2(50),
+    detalle_de_accion VARCHAR2(50),
+    fecha_hora TIMESTAMP,
+    datos_anteriores VARCHAR2(250),
+    datos_nuevos VARCHAR2(250),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
 );
 
-
 CREATE TABLE PERSONA (
-id_persona INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR2(100),
-apellido VARCHAR2(100),
-telefono VARCHAR2(100),
-direccion VARCHAR2(250),
+    id_persona NUMBER PRIMARY KEY,
+    nombre VARCHAR2(100),
+    apellido VARCHAR2(100),
+    telefono VARCHAR2(100),
+    direccion VARCHAR2(250),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
 );
 
 CREATE TABLE USUARIO (
-id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-id_persona INT,
-id_ rol INT,
-usuario VARCHAR2(100),
-correo VARCHAR2(100),
-contrasena VARCHAR2(100),
+    id_usuario NUMBER PRIMARY KEY,
+    id_persona NUMBER,
+    id_rol NUMBER,
+    usuario VARCHAR2(100),
+    correo VARCHAR2(100),
+    contrasena VARCHAR2(100),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
-FOREIGN KEY (id_rol) REFERENCES ROL(id_rol),
-FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona)
- );
-
-
- CREATE TABLE MODELO (
-id_modelo INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR2(100),
-anio INT,
-
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50),
+    CONSTRAINT fk_usuario_persona FOREIGN KEY (id_persona) REFERENCES PERSONA(id_persona),
+    CONSTRAINT fk_usuario_rol FOREIGN KEY (id_rol) REFERENCES ROL(id_rol)
 );
 
+CREATE TABLE MODELO (
+    id_modelo NUMBER PRIMARY KEY,
+    nombre VARCHAR2(100),
+    anio NUMBER,
+
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
+);
 
 CREATE TABLE MARCA_MODELO (
-id_marca INT AUTO_INCREMENT PRIMARY KEY,
-id_modelo INT,
-nombre VARCHAR2(100),
+    id_marca NUMBER PRIMARY KEY,
+    id_modelo NUMBER,
+    nombre VARCHAR2(100),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
-FOREIGN KEY (id_modelo) REFERENCES MODELO(id_modelo)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50),
+    CONSTRAINT fk_marca_modelo FOREIGN KEY (id_modelo) REFERENCES MODELO(id_modelo)
 );
 
 CREATE TABLE IMAGEN (
-id_imagen INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR2(100),
-url  VARCHAR2(4000),
+    id_imagen NUMBER PRIMARY KEY,
+    nombre VARCHAR2(100),
+    url  VARCHAR2(4000),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
 );
 
- CREATE TABLE TIPO_ESTADO_MOTOCICLETA (
-id_tipo_estado_motocicleta INT AUTO_INCREMENT PRIMARY KEY,
-descripcion VARCHAR2(1000),
+CREATE TABLE TIPO_ESTADO_MOTOCICLETA (
+    id_tipo_estado_motocicleta NUMBER PRIMARY KEY,
+    descripcion VARCHAR2(1000),
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
 );
-
 
 CREATE TABLE MOTOCICLETA (
-id_motocicleta INT AUTO_INCREMENT PRIMARY KEY,
-id_marca INT,
-id_imagen INT,
-id_tipo_estado_motocicleta INT,
-kilometraje INT,
-cilindraje INT,
-capacidad INT,
+    id_motocicleta NUMBER PRIMARY KEY,
+    id_marca NUMBER,
+    id_imagen NUMBER,
+    id_tipo_estado_motocicleta NUMBER,
+    kilometraje NUMBER,
+    cilindraje NUMBER,
+    capacidad NUMBER,
 
-fecha_creacion DATE,
-fecha_modificacion DATE,
-usuario_creo VARCHAR2(50),
-usuario_modifico VARCHAR2(50)
-FOREIGN KEY (id_marca) REFERENCES MARCA_MODELO(id_marca),
-FOREIGN KEY (id_imagen) REFERENCES IMAGEN(id_imagen),
-FOREIGN KEY (id_tipo_estado_motocicleta) REFERENCES TIPO_ESTADO_MOTOCICLETA(id_tipo_estado_motocicleta)
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50),
+    CONSTRAINT fk_motocicleta_marca FOREIGN KEY (id_marca) REFERENCES MARCA_MODELO(id_marca),
+    CONSTRAINT fk_motocicleta_imagen FOREIGN KEY (id_imagen) REFERENCES IMAGEN(id_imagen),
+    CONSTRAINT fk_motocicleta_estado FOREIGN KEY (id_tipo_estado_motocicleta) REFERENCES TIPO_ESTADO_MOTOCICLETA(id_tipo_estado_motocicleta)
 );
 
+CREATE TABLE ZONA_GEOGRAFICA (
+    id_zona_geografica NUMBER PRIMARY KEY,
+    nombre_zona  VARCHAR2(100),
+    codigo_postal NUMBER,
+    region  VARCHAR2(100),
+    ciudad  VARCHAR2(100),
+    direccion  VARCHAR2(100),
+
+    fecha_creacion DATE,
+    fecha_modificacion DATE,
+    usuario_creo VARCHAR2(50),
+    usuario_modifico VARCHAR2(50)
+);
